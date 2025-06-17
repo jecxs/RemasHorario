bb22235f058b056cb2e942d98697ebc60ee65431
Jeremy Castillo <josstinpretell3@gmail.com>
2025-06-17 16:04:16 -0500

    auditoria add 17-06

--- src/main/java/com/pontificia/remashorario/RemasHorarioApplication.java
+++ src/main/java/com/pontificia/remashorario/RemasHorarioApplication.java
@@ -2,8 +2,10 @@ package com.pontificia.remashorario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RemasHorarioApplication {
 
     public static void main(String[] args) {
